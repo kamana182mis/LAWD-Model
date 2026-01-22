a3d69b05bf7fa8dec1813ef3a97c401a73be9fa2
kamana182mis <62943719+kamana182mis@users.noreply.github.com>
2026-01-22 14:38:36 +0530

    Update Lawd regression simulation.R

--- Lawd regression simulation.R
+++ Lawd regression simulation.R
@@ -1,44 +1,57 @@
-rm(list=ls())
+rm(list = ls())
 library(doParallel)
 library(foreach)
 
-
-
-####################### Uncensored data #############################
-
-short_mle_analysis <- function(i_range, true_params, lower_bound, upper_bound, batch_size = 250) {
-  k_range <- 1000
-  num_batches <- ceiling(k_range / batch_size)
-  results <- matrix(0, ncol = length(true_params), nrow = k_range, dimnames = list(NULL, names(true_params)))
+short_mle_analysis <- function(i_range, true_params, lower_bound, upper_bound,
+                               batch_size = 250, k_range = 1000, max_attempts = 3) {
   
+  results <- matrix(NA, ncol = length(true_params), nrow = k_range,
+                    dimnames = list(NULL, names(true_params)))
+  conv_codes <- numeric(k_range)
+  attempts_per_rep <- numeric(k_range)
   
+  ####################### Data generator #######################
   gen_data <- function() {
-    ds <- c()  # Uncensored samples
-    q_u = 0.9
+    ds <- numeric(i_range)
+    q_u <- 0.9
     for (i in 1:i_range) {
-      sol <- function(x) {
-        zp <- rnorm(1)
-        meu <- exp(true_params['phi1'] + true_params['phi2'] * zp)
-        alph <- ((q_u / (1 - q_u))^(1 / true_params['eta']) - 
-                   true_params['beta'] * meu^true_params['gamma']) / meu^true_params['theta']
-        alph * x^true_params['theta'] + 
-          true_params['beta'] * x^true_params['gamma'] - 
-          ((u) / (1 - u))^(1 / true_params['eta'])
+      repeat {
+        u <- runif(1)
+        z <- rnorm(1)
+        sol <- function(x) {
+          meu <- exp(true_params['phi1'] + true_params['phi2'] * z)
+          alph <- ((q_u / (1 - q_u))^(1 / true_params['eta']) - 
+                     true_params['beta'] * meu^true_params['gamma']) / meu^true_params['theta']
+          alph * x^true_params['theta'] + 
+            true_params['beta'] * x^true_params['gamma'] - ((u) / (1 - u))^(1 / true_params['eta'])
+        }
+        # safe uniroot
+        lower <- 1e-4; upper <- 50
+        f1 <- sol(lower); f2 <- sol(upper)
+        expand <- 1
+        while (f1 * f2 > 0 && expand <= 6) {
+          upper <- upper * 2
+          f2 <- sol(upper)
+          expand <- expand + 1
+        }
+        if (f1 * f2 < 0 && is.finite(f1) && is.finite(f2)) {
+          root <- try(uniroot(sol, c(lower, upper))$root, silent = TRUE)
+          if (!inherits(root, "try-error") && is.finite(root)) {
+            ds[i] <- root
+            break
+          }
+        }
       }
-      u <- runif(1)
-      sample <- uniroot(sol, c(0.001, 100), extendInt = "yes")$root
-      ds <- c(ds, sample)
     }
     return(list(ds = ds))
   }
   
+  ####################### Log-likelihood #######################
   log_likelihood <- function(x, data) {
     the <- x[1]; bet <- x[2]; gam <- x[3]; et <- x[4]; ph1 <- x[5]; ph2 <- x[6]
-    q_u = 0.9
-    ds <- data[[1]]  # Uncensored samples
-    sum_ds <- 0
-    sum1_ds <- 0
-    sum2_ds <- 0
+    q_u <- 0.9
+    ds <- data[[1]]
+    sum_ds <- 0; sum1_ds <- 0; sum2_ds <- 0
     for (j in 1:length(ds)) {
       z1 <- rnorm(1)
       meu <- exp(ph1 + ph2 * z1)
@@ -47,70 +60,120 @@ short_mle_analysis <- function(i_range, true_params, lower_bound, upper_bound, b
         sum_ds <- sum_ds + log(alp * the * ds[j]^(the - 1) + bet * gam * ds[j]^(gam - 1))
         sum1_ds <- sum1_ds + log(alp * ds[j]^the + bet * ds[j]^gam)
         sum2_ds <- sum2_ds + log(1 + (alp * ds[j]^the + bet * ds[j]^gam)^et)
-      } else {
-        return(-1e10)  # Penalty
-      }
+      } else return(-1e10)
     }
     ff <- length(ds) * log(et) + sum_ds + (et - 1) * sum1_ds - 2 * sum2_ds
     return(ff)
   }
   
-  # Set up parallel backend
-  num_cores <- detectCores() - 1  
+  ####################### Parallel setup #######################
+  num_cores <- detectCores() - 1
   cl <- makeCluster(num_cores)
   registerDoParallel(cl)
-  clusterSetRNGStream(cl, 24) 
+  clusterSetRNGStream(cl, 24)
   
-  # Run the analysis in batches
+  ####################### Run simulations #######################
+  num_batches <- ceiling(k_range / batch_size)
+  
   for (batch in 1:num_batches) {
     batch_start <- (batch - 1) * batch_size + 1
     batch_end <- min(batch * batch_size, k_range)
+    
     batch_results <- foreach(k = batch_start:batch_end, .combine = rbind, .packages = "stats") %dopar% {
-      successful <- FALSE
-      result <- NULL
-      while (!successful) {
+      
+      attempt <- 1
+      repeat {
         data <- gen_data()
         optim_result <- optim(true_params, log_likelihood, data = data,
                               lower = lower_bound, upper = upper_bound,
-                              method = "L-BFGS-B", control = list(fnscale = -1), hessian = TRUE)
-        if (optim_result$value > -1e10) {  
-          successful <- TRUE
-          result <- optim_result$par
+                              method = "L-BFGS-B", control = list(fnscale = -1))
+        
+        if (optim_result$convergence == 0 || attempt >= max_attempts) {
+          break
+        } else {
+          attempt <- attempt + 1
         }
       }
-      return(result)
+      
+      c(optim_result$par, conv_code = optim_result$convergence, attempts = attempt)
     }
     
-    results[batch_start:batch_end, ] <- batch_results
-    print(paste("Batch", batch, "completed"))
+    results[batch_start:batch_end, ] <- batch_results[, 1:length(true_params)]
+    conv_codes[batch_start:batch_end] <- batch_results[, "conv_code"]
+    attempts_per_rep[batch_start:batch_end] <- batch_results[, "attempts"]
+    
+    cat("Batch", batch, "completed\n")
   }
   
   stopCluster(cl)
   
+  ####################### Compute proportion converged #######################
+  convergence_rate <- mean(conv_codes == 0)
+  
+  ####################### Summary statistics #######################
   est_list <- list()
   summary_stats <- sapply(seq_along(true_params), function(i) {
     est <- results[, i]
-    est_list[[names(true_params)[i]]] <<- est  
+    est_list[[names(true_params)[i]]] <<- est
     rmse <- sqrt(mean((est - true_params[i])^2))
-    sd <- sd(est)
+    sd_val <- sd(est)
     rel_bias <- mean(abs(est - true_params[i]) / true_params[i])
-    c(mean = mean(est), rmse = rmse, sd = sd, rel_bias = rel_bias)
+    c(mean = mean(est), rmse = rmse, sd = sd_val, rel_bias = rel_bias)
   })
   
   list(summary_stats = as.data.frame(t(summary_stats), row.names = names(true_params)),
-       estimates = est_list)  
+       estimates = est_list,
+       convergence = list(convergence_rate = convergence_rate,
+                          conv_codes = conv_codes,
+                          attempts = attempts_per_rep))
 }
-
-
-
 tp <- c(theta = 1.8, beta = 1.3, gamma = 0.5, eta = 1.1, phi1 = 0.8, phi2 = 0.7)
-lb = tp - 0.49
-ub = tp + 0.51
-
+lb <- tp - 0.49
+ub <- tp + 0.51
 
 
 results6 <- short_mle_analysis(i_range = 20, tp, lb, ub)
 results6$summary_stats
+
+
+estimates <- results6$estimates
+params <- names(estimates)
+
+table(results6$convergence$attempts)[[1]]  #986
+
+# Mapping parameter names to math expressions
+param_expr <- list(
+  theta = expression(theta),
+  beta  = expression(beta),
+  gamma = expression(gamma),
+  eta   = expression(eta),
+  phi1  = expression(omega[1]),
+  phi2  = expression(omega[2])
+)
+
+params <- names(estimates)
+
+png("MLE_estimates_traceplots_cp1.png", width = 3000, height =2400, res = 300)
+
+par(mfrow = c(2,3), mar = c(4.5, 5, 4, 1))  # layout + margins
+
+for (p in params) {
+  plot(estimates[[p]], type = "l",
+       main = param_expr[[p]],
+       xlab = "Replications",
+       ylab = "Estimated value",
+       col = "blue",
+       cex.lab = 1.3,     # xlab & ylab size
+       cex.axis = 1.3,    # tick size
+       cex.main = 1.5,    # title size
+       lwd = 1)
+  
+  abline(h = tp[p], col = "red", lty = 2, lwd = 2)
+}
+
+dev.off()
+
+
 
 results7 <- short_mle_analysis(i_range = 30, tp, lb, ub)
 results7$summary_stats
@@ -127,32 +190,56 @@ results3 <- short_mle_analysis(i_range = 300, tp, lb, ub)
 results3$summary_stats
 
 
+table(results6$convergence$attempts)[[1]]
+
+table(results7$convergence$attempts)[[1]]
+
+table(results0$convergence$attempts)[[1]]
+
+table(results1$convergence$attempts)[[1]]
+
+table(results3$convergence$attempts)[[1]]
+
+mean(results6$convergence$attempts);  mean(results7$convergence$attempts);  
+mean(results0$convergence$attempts);  mean(results1$convergence$attempts)
+mean(results3$convergence$attempts)
+
+
 ####################### Censored data #############################
 
-short_mle_analysis <- function(i_range, true_params, lower_bound, upper_bound, batch_size = 250) {
-  k_range <- 1000
+
+short_mle_analysis <- function(i_range, true_params, lower_bound, upper_bound,
+                               batch_size = 250, k_range = 1000, max_attempts = 3) {
+  
   num_batches <- ceiling(k_range / batch_size)
-  results <- matrix(0, ncol = length(true_params), nrow = k_range, dimnames = list(NULL, names(true_params)))
   
+  results <- matrix(NA, ncol = length(true_params), nrow = k_range,
+                    dimnames = list(NULL, names(true_params)))
+  conv_codes <- numeric(k_range)
+  attempts_per_rep <- numeric(k_range)
   
+  ####################### Data generator #######################
   gen_data <- function() {
-    ds <- c()  # Uncensored samples
-    dc <- c()  # Censored samples
-    q_u = 0.9
+    ds <- c()  # Uncensored
+    dc <- c()  # Censored
+    q_u <- 0.9
+    
     for (i in 1:i_range) {
+      
       sol <- function(x) {
         zp <- rnorm(1)
         meu <- exp(true_params['phi1'] + true_params['phi2'] * zp)
-        alph <- ((q_u / (1 - q_u))^(1 / true_params['eta']) - 
+        alph <- ((q_u / (1 - q_u))^(1 / true_params['eta']) -
                    true_params['beta'] * meu^true_params['gamma']) / meu^true_params['theta']
-        alph * x^true_params['theta'] + 
-          true_params['beta'] * x^true_params['gamma'] - 
+        alph * x^true_params['theta'] +
+          true_params['beta'] * x^true_params['gamma'] -
           ((u) / (1 - u))^(1 / true_params['eta'])
       }
+      
       u <- runif(1)
       sample <- uniroot(sol, c(0.001, 100), extendInt = "yes")$root
       
-      u <- 0.7                                                           #### change censoring cp = 1-u
+      u <- 0.7  # censoring level
       t_yc <- uniroot(sol, c(0.001, 100), extendInt = "yes")$root
       censor_time <- runif(1, 0, t_yc)
       
@@ -167,97 +254,158 @@ short_mle_analysis <- function(i_range, true_params, lower_bound, upper_bound, b
   }
   
   log_likelihood <- function(x, data) {
-    the <- x[1]; bet <- x[2]; gam <- x[3]; et <- x[4]; ph1 <- x[5]; ph2 <- x[6]
-    q_u = 0.9
-    ds <- data[[1]]  # Uncensored samples
-    dc <- data[[2]]  # Censored samples
-    sum_ds <- 0
-    sum1_ds <- 0
-    sum2_ds <- 0
+    the <- x[1]; bet <- x[2]; gam <- x[3]
+    et  <- x[4]; ph1 <- x[5]; ph2 <- x[6]
+    q_u <- 0.9
+    
+    ds <- data[[1]]
+    dc <- data[[2]]
+    
+    sum_ds <- 0; sum1_ds <- 0; sum2_ds <- 0
+    
     for (j in 1:length(ds)) {
       z1 <- rnorm(1)
       meu <- exp(ph1 + ph2 * z1)
       alp <- ((q_u / (1 - q_u))^(1 / et) - bet * meu^gam) / meu^the
+      
       if (alp > 0) {
-        sum_ds <- sum_ds + log(alp * the * ds[j]^(the - 1) + bet * gam * ds[j]^(gam - 1))
+        sum_ds  <- sum_ds  + log(alp * the * ds[j]^(the - 1) + bet * gam * ds[j]^(gam - 1))
         sum1_ds <- sum1_ds + log(alp * ds[j]^the + bet * ds[j]^gam)
         sum2_ds <- sum2_ds + log(1 + (alp * ds[j]^the + bet * ds[j]^gam)^et)
-      } else {
-        return(-1e10)  # Penalty
-      }
+      } else return(-1e10)
     }
+    
     sum3_dc <- 0
     for (k in 1:length(dc)) {
       z1 <- rnorm(1)
       meu <- exp(ph1 + ph2 * z1)
       alp <- ((q_u / (1 - q_u))^(1 / et) - bet * meu^gam) / meu^the
+      
       if (alp > 0) {
         sum3_dc <- sum3_dc + log(1 + (alp * dc[k]^the + bet * dc[k]^gam)^et)
-      } else {
-        return(-1e10)  # Penalty
-      }
+      } else return(-1e10)
     }
-    ff <- length(ds) * log(et) + sum_ds + (et - 1) * sum1_ds - 2 * sum2_ds
+    
+    ff  <- length(ds) * log(et) + sum_ds + (et - 1) * sum1_ds - 2 * sum2_ds
     ffc <- -sum3_dc
+    
     return(ff + ffc)
   }
   
-  # Set up parallel backend
+  ####################### Parallel setup #######################
   num_cores <- detectCores() - 1  
   cl <- makeCluster(num_cores)
   registerDoParallel(cl)
   clusterSetRNGStream(cl, 24) 
   
-  # Run the analysis in batches
+  ####################### Run simulations #######################
   for (batch in 1:num_batches) {
-    batch_start <- (batch - 1) * batch_size + 1
-    batch_end <- min(batch * batch_size, k_range)
-    batch_results <- foreach(k = batch_start:batch_end, .combine = rbind, .packages = "stats") %dopar% {
-      successful <- FALSE
-      result <- NULL
-      while (!successful) {
-        data <- gen_data()
-        optim_result <- optim(true_params, log_likelihood, data = data,
-                              lower = lower_bound, upper = upper_bound,
-                              method = "L-BFGS-B", control = list(fnscale = -1), hessian = TRUE)
-        if (optim_result$value > -1e10) {  
-          successful <- TRUE
-          result <- optim_result$par
-        }
-      }
-      return(result)
-    }
     
-    results[batch_start:batch_end, ] <- batch_results
-    print(paste("Batch", batch, "completed"))
+    batch_start <- (batch - 1) * batch_size + 1
+    batch_end   <- min(batch * batch_size, k_range)
+    
+    batch_results <- foreach(k = batch_start:batch_end, .combine = rbind,
+                             .packages = "stats") %dopar% {
+                               
+                               attempt <- 1
+                               
+                               repeat {
+                                 data <- gen_data()
+                                 
+                                 optim_result <- optim(true_params, log_likelihood, data = data,
+                                                       lower = lower_bound, upper = upper_bound,
+                                                       method = "L-BFGS-B",
+                                                       control = list(fnscale = -1))
+                                 
+                                 if (optim_result$convergence == 0 || attempt >= max_attempts) break
+                                 attempt <- attempt + 1
+                               }
+                               
+                               c(optim_result$par,
+                                 conv_code = optim_result$convergence,
+                                 attempts  = attempt)
+                             }
+    
+    results[batch_start:batch_end, ] <- batch_results[, 1:length(true_params)]
+    conv_codes[batch_start:batch_end] <- batch_results[, "conv_code"]
+    attempts_per_rep[batch_start:batch_end] <- batch_results[, "attempts"]
+    
+    cat("Batch", batch, "completed\n")
   }
   
   stopCluster(cl)
-  
+  convergence_rate <- mean(conv_codes == 0)
+
   est_list <- list()
+  
   summary_stats <- sapply(seq_along(true_params), function(i) {
     est <- results[, i]
-    est_list[[names(true_params)[i]]] <<- est  
+    est_list[[names(true_params)[i]]] <<- est
+    
     rmse <- sqrt(mean((est - true_params[i])^2))
-    sd <- sd(est)
+    sdv  <- sd(est)
     rel_bias <- mean(abs(est - true_params[i]) / true_params[i])
-    c(mean = mean(est), rmse = rmse, sd = sd, rel_bias = rel_bias)
+    
+    c(mean = mean(est), rmse = rmse, sd = sdv, rel_bias = rel_bias)
   })
   
   list(summary_stats = as.data.frame(t(summary_stats), row.names = names(true_params)),
-       estimates = est_list)  
+       estimates = est_list,
+       convergence = list(convergence_rate = convergence_rate,
+                          conv_codes = conv_codes,
+                          attempts = attempts_per_rep))
 }
 
 
+tp <- c(theta = 1.8, beta = 1.3, gamma = 0.5,
+        eta = 1.1, phi1 = 0.8, phi2 = 0.7)
 
-tp <- c(theta = 1.8, beta = 1.3, gamma = 0.5, eta = 1.1, phi1 = 0.8, phi2 = 0.7)
-lb = tp - 0.49
-ub = tp + 0.51
-
-
+lb <- tp - 0.49
+ub <- tp + 0.51
 
 results6 <- short_mle_analysis(i_range = 20, tp, lb, ub)
+
 results6$summary_stats
+results6$convergence$convergence_rate
+
+table(results6$convergence$attempts)[[1]]
+
+estimates <- results6$estimates
+params <- names(estimates)
+
+table(results6$convergence$attempts)[[1]]  #986
+
+# Mapping parameter names to math expressions
+param_expr <- list(
+  theta = expression(theta),
+  beta  = expression(beta),
+  gamma = expression(gamma),
+  eta   = expression(eta),
+  phi1  = expression(omega[1]),
+  phi2  = expression(omega[2])
+)
+
+params <- names(estimates)
+
+png("MLE_estimates_traceplots_cp1.png", width = 3000, height =2400, res = 300)
+
+par(mfrow = c(2,3), mar = c(4.5, 5, 4, 1))  # layout + margins
+
+for (p in params) {
+  plot(estimates[[p]], type = "l",
+       main = param_expr[[p]],
+       xlab = "Replications",
+       ylab = "Estimated value",
+       col = "blue",
+       cex.lab = 1.3,     # xlab & ylab size
+       cex.axis = 1.3,    # tick size
+       cex.main = 1.5,    # title size
+       lwd = 1)
+  
+  abline(h = tp[p], col = "red", lty = 2, lwd = 2)
+}
+
+dev.off()
 
 results7 <- short_mle_analysis(i_range = 30, tp, lb, ub)
 results7$summary_stats
